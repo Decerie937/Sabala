69b977ae0a0c07605f71bcb533848296e0665499
Decerie937 <deceriekatesabala@gmail.com>
2025-03-10 07:47:00 +0800

    Add files via upload

---
+++ Walk-through 5.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int[][] dataTable = {
+            {3, 6, 9},
+            {12, 15, 18},
+            {21, 24, 27}
+        };
+
+        
+        System.out.println("Data Table:");
+        for (int row = 0; row < dataTable.length; row++) {
+            for (int col = 0; col < dataTable[row].length; col++) {
+                System.out.print(dataTable[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
